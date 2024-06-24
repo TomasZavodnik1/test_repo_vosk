3342bf1b583c54e69e229a9bed3e484c25f4693f
Tomas Zavodnik <tomas.zavodnik@tii.ae>
2024-06-24 08:11:28 +0400

    sync

--- packages/vosk/kaldi/default.nix
+++ packages/vosk/kaldi/default.nix
@@ -18,7 +18,7 @@
 #with import <nixpkgs> {};
 with pkgs;
 stdenv.mkDerivation rec {
-  pname = "vosk-kaldi-openblas";
+  pname = "kaldi";
   version = "v0.3.50";
   src = ./.;
   buildInputs = [  glibc.dev gcc libgcc gnumake pkgs.python39.pkgs.setuptools pkgs.python39.pkgs.cffi pkgs.python39.pkgs.requests pkgs.python39.pkgs.tqdm pkgs.python39.pkgs.srt pkgs.python39.pkgs.websockets  pkgs.python39.pkgs.setuptools coreutils bashInteractiveFHS dash bash  cmake  gfortran.cc perl bash python39 autoconf automake zlib unzip sox libtool ];
@@ -30,7 +30,7 @@ stdenv.mkDerivation rec {
   doCheck=false;
   
   buildPhase = ''
-                tar -xvf source.tar
+                #tar -xvf source.tar
                 cd tools/
                 make openfst cub
                 cd extras/OpenBLAS                
