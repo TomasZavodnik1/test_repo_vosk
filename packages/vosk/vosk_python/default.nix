f9f60601102062d95dff249bea3dafe566e4b1c5
Tomas Zavodnik <tomas.zavodnik@tii.ae>
2024-06-30 22:50:36 +0400

    sync

--- packages/vosk/vosk_python/default.nix
+++ packages/vosk/vosk_python/default.nix
@@ -7,8 +7,8 @@ in
 buildPythonPackage {
   pname = "vosk-python";
   version = "1.0";
-  phases = [ "unpackPhase" ];
-  unpackPhase = ''  export VOSK_SOURCE=${vosk-pkg}  '';
+  #phases = [ "unpackPhase" ];
+  #unpackPhase = ''  export VOSK_SOURCE=${vosk-pkg}  '';
   nativeBuildInputs = [ vosk-pkg cffi requests tqdm srt websockets ];
   buildInputs = [ vosk-pkg cffi requests tqdm srt websockets ];
   src = ./.;
