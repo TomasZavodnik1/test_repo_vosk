b4efd3a8f4bde2171c9da85ce52aaa8de564823b
Tomas Zavodnik <tomas.zavodnik@tii.ae>
2024-07-01 00:32:54 +0400

    sync

--- packages/vosk/vosk_python_test/default.nix
+++ packages/vosk/vosk_python_test/default.nix
@@ -5,7 +5,7 @@ pkgs.python311Packages.buildPythonApplication {
   pname = "vosk-python-test";
   version = "1.0";
   
-  nativeBuildInputs = [ (pkgs.callPackage ../vosk_python {}) pkgs.python311Packages.pkgs.cffi pkgs.python311Packages.pkgs.requests pkgs.python311Packages.pkgs.tqdm pkgs.python311Packages.pkgs.srt pkgs.python311Packages.pkgs.websockets ];
+  nativeBuildInputs = [ (pkgs.callPackage ../vosk_python {}) pkgs.python311Packages.cffi pkgs.python311Packages.pkgs.requests pkgs.python311Packages.pkgs.tqdm pkgs.python311Packages.pkgs.srt pkgs.python311Packages.pkgs.websockets ];
   buildInputs = [ (pkgs.callPackage ../vosk_python {}) pkgs.python311Packages.pkgs.cffi pkgs.python311Packages.pkgs.requests pkgs.python311Packages.pkgs.tqdm pkgs.python311Packages.pkgs.srt pkgs.python311Packages.pkgs.websockets ];
   src = ./.;
 
