1b06537c478f22b5641c10374191d3786961e13a
Tomas Zavodnik <tomas.zavodnik@tii.ae>
2024-07-01 14:54:33 +0400

    sync

--- packages/vosk/kaldi-openfst-openblas/subst_script.sh
+++ packages/vosk/kaldi-openfst-openblas/subst_script.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-VAR='@bash@';
+VAR='%bash%';
 VAR=$(echo $VAR | sed 's=/=\\/=g'); 
 
-grep -rl '@bash@' | xargs sed -i "s/@bash@/{$VAR}/g";
+grep -rl '{@bash@}' | xargs sed -i "s/@bash@/$VAR/g";
