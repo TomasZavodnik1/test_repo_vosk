7335ba79e28b2ff1e56bbcab352d13ad26d3d6d2
Tomas Zavodnik <tomas.zavodnik@tii.ae>
2024-07-09 09:48:18 +0400

    sync

--- packages/vosk/vosk_python/default.nix
+++ packages/vosk/vosk_python/default.nix
@@ -10,7 +10,9 @@ pkgs.python311Packages.buildPythonPackage {
   pname = "vosk-python";
   version = "1.0";
   #phases = [ "unpackPhase" ];
-  preBuild = ''  export VOSK_SOURCE=${vosk-pkg}  '';
+  preBuild = ''  export VOSK_SOURCE=${vosk-pkg};
+                 substituteInPlace vosk_builder.py  --replace "$cc" ${cc}
+  '';
 
   nativeBuildInputs = [ pkgs.python311Packages.wavefile vosk-pkg pkgs.python311Packages.cffi pkgs.python311Packages.requests pkgs.python311Packages.tqdm pkgs.python311Packages.srt pkgs.python311Packages.websockets pkgs.python311Packages.srt pkgs.python311Packages.zipfile2 pkgs.python311Packages.pyzipper ];
   propagatedBuildInputs = [ pkgs.python311Packages.wavefile vosk-pkg pkgs.python311Packages.cffi pkgs.python311Packages.requests pkgs.python311Packages.tqdm pkgs.python311Packages.srt pkgs.python311Packages.websockets pkgs.python311Packages.srt pkgs.python311Packages.zipfile2 pkgs.python311Packages.pyzipper  ];
